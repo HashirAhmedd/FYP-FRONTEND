e698c3315cfdf9f02415aa5f2a424906b3b0c556
Hashir <hashirahmed868@gmail.com>
2025-12-17 21:32:55 +0500

    added authentication modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
 import Layout from "./Layout/Layout";
+import AuthModal from "./Pages/AuthModal";
+
 export default function App() {
+  const [isUser, setIsUser] = useState(false);
+
   return (
-    <div className="bg-gray-50">
-      <Layout />
-    </div>
+    <>
+      {!isUser ? (
+        <AuthModal setIsUser={setIsUser} />
+      ) : (
+        <div className="bg-gray-50">
+          <Layout />
+        </div>
+      )}
+    </>
   );
 }
